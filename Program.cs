ff3b7916de1f6f8498d2be7b12b58ce80882f2b6
sebainones <seba.inones@gmail.com>
2020-08-29 18:54:46 +0200

    Basic structure and dependencies added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniunNetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
