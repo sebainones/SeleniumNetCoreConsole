caa73129b8cbbe6d5d9300e96ae812596c382992
sebainones <seba.inones@gmail.com>
2020-08-29 20:30:23 +0200

    Completed a ver basic example of how to use selenium

---
+++ PageRepository/DuckDuckGoHomePage.cs
@@ -0,0 +1,10 @@
+namespace SeleniunNetCoreConsoleApp.PageRepository
+{
+    public class DuckDuckGoHomePage
+    {
+        public static string SearchBoxId = @"search_form_input_homepage";
+        public static string SearchButtonId = @"search_button_homepage";        
+
+        public static string StackOverFlowLinkText =>@"User Sebastian Inones - Stack Overflow";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniunNetCoreConsoleApp.PageRepository;
 
 namespace SeleniunNetCoreConsoleApp
 {
@@ -8,16 +11,38 @@ namespace SeleniunNetCoreConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello RPA with Selenium!");
+            
+            ChromeDriver driver = null;
+
             try 
             {
-                var driver = new ChromeDriver(@"/home/sebainones/Utils/");
-
+                 driver = new ChromeDriver(@"/home/sebainones/Utils/");
 
                 driver.Navigate().GoToUrl("https://duckduckgo.com/");
+
+                var searchBox =  driver.FindElementById(DuckDuckGoHomePage.SearchBoxId);
+                if(searchBox != null)
+                {
+                    searchBox.SendKeys("Sebastian Inones");
+
+                    var searchButon = driver.FindElementById(DuckDuckGoHomePage.SearchButtonId);
+                    searchButon.Click();                    
+
+                    var actualLinkReult = driver.FindElement(By.LinkText(DuckDuckGoHomePage.StackOverFlowLinkText));
+                    if(actualLinkReult != null)
+                    {
+                        actualLinkReult.Click();
+                    }                    
+                }
             }
             catch(Exception exception)
             {
                 Console.WriteLine(exception.Message);
+            }
+            finally
+            {
+                if(driver != null)
+                    driver.Close();
             }
     
         }
